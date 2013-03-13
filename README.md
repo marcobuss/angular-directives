0ac5e749a5e80e0bb64f5966994f7b6b0fd1cd00
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-03-13 10:00:33 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+Sample project to introduce angular directives.
+
+See [Writing Directives](http://youtu.be/WqmeI5fZcho)
+See [Testing Directives](http://youtu.be/rB5b67Cg6bc)
---
+++ app/js/directives.js
@@ -0,0 +1,12 @@
+var app = angular.module("directives", []);
+
+app.directive('axHello', function() {
+    return {
+        restrict: 'A', // Default?
+        scope: {
+            greeted: '@',
+            greeter: '='
+        },
+        template: '<p>Hello {{greeted}} from {{greeter}}!!!</p>'
+    }
+});
