891cf023a22aa303819c81c27feda4fe347c03db
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-03-13 10:06:09 +0100

    Fixed wrong baseline for directives.js.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,12 +1,1 @@
 var app = angular.module("directives", []);
-
-app.directive('axHello', function() {
-    return {
-        restrict: 'A', // Default?
-        scope: {
-            greeted: '@',
-            greeter: '='
-        },
-        template: '<p>Hello {{greeted}} from {{greeter}}!!!</p>'
-    }
-});
