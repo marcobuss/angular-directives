6de84931b7f5553bf6fbb4903140565cd63b8bb2
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-03-13 10:09:10 +0100

    1 - static hello world directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,1 +1,9 @@
 var app = angular.module("directives", []);
+
+app.directive('axHello', function() {
+    return {
+        restrict: 'A', // Default
+        template: '<p>Hello World!</p>'
+        // ,replace: true
+    }
+});
