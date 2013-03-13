b89a7e4b04d62af2c7a8679018a3acc8ec287223
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-03-13 10:09:59 +0100

    2 - use enclosing scope property.

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,6 @@ var app = angular.module("directives", []);
 app.directive('axHello', function() {
     return {
         restrict: 'A', // Default
-        template: '<p>Hello World!</p>'
-        // ,replace: true
+        template: '<p>Hello {{username}}!</p>'
     }
 });
