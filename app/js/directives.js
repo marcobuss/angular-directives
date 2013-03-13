1c427f22a319420c1171a2cd48d6573cac26894d
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2013-03-13 10:10:53 +0100

    3 - introduce isolate scope.

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,6 +3,7 @@ var app = angular.module("directives", []);
 app.directive('axHello', function() {
     return {
         restrict: 'A', // Default
+        scope: {},
         template: '<p>Hello {{username}}!</p>'
     }
 });
